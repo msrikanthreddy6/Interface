904c6fc46ab0d464442aca94c4c547955128276f
msrikanthreddy6 <msrikanthreddy6@gmail.com>
2022-07-12 17:29:56 +0530

    New Commit

--- My_PC/My_PC/src/pac1/LeftDownMirrorPattern.java
+++ My_PC/My_PC/src/pac1/LeftDownMirrorPattern.java
@@ -3,7 +3,6 @@ package pac1;
 public class LeftDownMirrorPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int row = 10;
 		for (int i = 1; i <= row; i++) {
 			for (int j = row; j >= i; j++) {
