91d4ba463c0be44d78449947a877ac678bda3394
M Srikanth Reddy <msrikanthreddy6@gmail.com>
2023-04-04 15:58:17 +0530

    all code added

--- WebDriver/src/pac1/Navigate_Methods.java
+++ WebDriver/src/pac1/Navigate_Methods.java
@@ -1,41 +1,43 @@
 package pac1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Navigate_Methods {
 
+	private static WebDriver driver;
+
 	public static void main(String[] args) throws InterruptedException {
 
-		System.setProperty("webdriver.chrome.driver", "D:\\Browser drivers\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.facebook.com/");
-		
-		
-		Thread.sleep(3000);
-		driver.navigate().refresh();
-		driver.manage().window().maximize();
-		
-		String etitle="Facebook – log in or sign up";
-		String atitle=driver.getTitle();
-		
-		if(etitle.equals(atitle))
-		{
-			System.out.println(driver.getTitle());
-			Thread.sleep(3000);
-			driver.navigate().back();
-			Thread.sleep(3000);
-			driver.navigate().forward();
-		}
-		else
-		{
-			System.out.println("Invalid Application");
-			driver.close();
-		}
-		
-		
-		
+		try {
+			System.setProperty("webdriver.chrome.driver", "D:/chromedriver_win32/chromedriver.exe");
+			 driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.facebook.com/");
 
+			String etitle = "Facebook – log in or sign up";
+			String atitle = driver.getTitle();
+
+			if (etitle.equals(atitle)) {
+				System.out.println(driver.getTitle());
+				Thread.sleep(3000);
+				driver.findElement(By.id("//input[@id='email']")).sendKeys("9502942103");
+				Thread.sleep(3000);
+				driver.findElement(By.id("//input[@id='pass']")).sendKeys("Srikanth$234");
+				Thread.sleep(3000);
+				// driver.findElement(By.id("//button[@id='u_0_5_ww']")).click();
+				driver.navigate().back();
+				Thread.sleep(3000);
+				driver.navigate().forward();
+			} else {
+				System.out.println("Invalid Application");
+				driver.close();
+			}
+
+		} catch (Exception e) {
+			driver.quit();
+		}
 	}
 
 }
